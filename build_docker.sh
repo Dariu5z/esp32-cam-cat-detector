37d4e2a5d891eb94c318d3d0ad0354742af2844e
Dariusz Syc <dariusz.syc@googlemail.com>
2025-12-02 23:01:39 +0100

    cmds: fix build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -3,27 +3,15 @@
 set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-WORK_DIR=$(mktemp -d -p "$DIR")
-
-if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
-    echo "Could not create temp dir"
-    exit 1
-fi
-
-cleanup() {
-    rm -rf "$WORK_DIR"
-    echo "Deleted temp working directory \"$WORK_DIR\""
-}
-
-trap cleanup EXIT
 
 MY_USER_LOWERCASE=$(echo "$USER" | tr '[:upper:]' '[:lower:]')
 MY_UID=$(id -u "$MY_USER_LOWERCASE")
 
 docker build \
+    --no-cache \
     -f "$DIR/Dockerfile" \
     -t "cat_detector_$MY_USER_LOWERCASE:latest" \
     --build-arg USERNAME="$MY_USER_LOWERCASE" \
     --build-arg USER_UID="$MY_UID" \
     --build-arg USER_GID="$MY_UID" \
-    "$WORK_DIR"
+    "$DIR"
